e951fb6a9659259c543938f5ec04d18e01a97d15
liyanjun <41986035@qq.com>
2018-10-09 17:32:01 +0800

    添加https配置

--- src/main/java/com/funny/FunnyApplication.java
+++ src/main/java/com/funny/FunnyApplication.java
@@ -1,8 +1,15 @@
 package com.funny;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -14,6 +21,12 @@ import org.springframework.web.client.RestTemplate;
 @MapperScan("com.funny.admin.*.dao")
 public class FunnyApplication {
 
+	@Value("${server.port}")
+	private int serverPort;
+
+	@Value("${server.http.port}")
+	private int serverHttpPort;
+
 	public static void main(String[] args) {
 		SpringApplication.run(FunnyApplication.class, args);
 	}
@@ -21,5 +34,34 @@ public class FunnyApplication {
 	@Bean
 	public RestTemplate getRestTemplate(){
 		return new RestTemplate();
+	}
+
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+
+		tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+		return tomcat;
+	}
+
+	private Connector initiateHttpConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		//需要重定向的http端口
+		connector.setPort(serverHttpPort);
+		connector.setSecure(false);
+		//设置重定向到https端口
+		connector.setRedirectPort(serverPort);
+		return connector;
 	}
 }
