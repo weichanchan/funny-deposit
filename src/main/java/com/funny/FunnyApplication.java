2ffc663c3421eaa26b5bcab31175476fd71263c4
liyanjun <41986035@qq.com>
2018-10-10 16:14:46 +0800

    悲惨的配置问题

--- src/main/java/com/funny/FunnyApplication.java
+++ src/main/java/com/funny/FunnyApplication.java
@@ -8,6 +8,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.cache.annotation.EnableCaching;
@@ -21,12 +22,6 @@ import org.springframework.web.client.RestTemplate;
 @MapperScan("com.funny.admin.*.dao")
 public class FunnyApplication {
 
-	@Value("${server.port}")
-	private int serverPort;
-
-	@Value("${server.http.port}")
-	private int serverHttpPort;
-
 	public static void main(String[] args) {
 		SpringApplication.run(FunnyApplication.class, args);
 	}
@@ -36,32 +31,4 @@ public class FunnyApplication {
 		return new RestTemplate();
 	}
 
-	@Bean
-	public EmbeddedServletContainerFactory servletContainer() {
-		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
-			@Override
-			protected void postProcessContext(Context context) {
-				SecurityConstraint securityConstraint = new SecurityConstraint();
-				securityConstraint.setUserConstraint("CONFIDENTIAL");
-				SecurityCollection collection = new SecurityCollection();
-				collection.addPattern("/*");
-				securityConstraint.addCollection(collection);
-				context.addConstraint(securityConstraint);
-			}
-		};
-
-		tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
-		return tomcat;
-	}
-
-	private Connector initiateHttpConnector() {
-		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-		connector.setScheme("http");
-		//需要重定向的http端口
-		connector.setPort(serverHttpPort);
-		connector.setSecure(false);
-		//设置重定向到https端口
-		connector.setRedirectPort(serverPort);
-		return connector;
-	}
 }
---
+++ src/main/java/com/funny/utils/WebConfig.java
@@ -0,0 +1,67 @@
+package com.funny.utils;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@ConditionalOnProperty("server.http.port")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Value("${server.port}")
+    private int serverPort;
+
+    @Value("${server.http.port}")
+    private int serverHttpPort;
+
+    /**
+     * 解决跨域问题
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT")
+                .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+                        "Access-Control-Request-Headers","accessToken")
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                .allowCredentials(true).maxAge(3600);
+    }
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+        return tomcat;
+    }
+
+    private Connector initiateHttpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        //需要重定向的http端口
+        connector.setPort(serverHttpPort);
+        connector.setSecure(false);
+        //设置重定向到https端口
+        connector.setRedirectPort(serverPort);
+        return connector;
+    }
+}
